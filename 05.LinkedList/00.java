6dbcb42002369ef6cf075d6b7d919b6f89b304db
Rahul Tilwani <tilwani03@gmail.com>
2021-06-29 18:00:53 +0530

    .

---
+++ 05.LinkedList/00.java
@@ -0,0 +1,42 @@
+import helpers.ListNode;
+
+// https://leetcode.com/problems/reverse-linked-list/
+
+// ITERATIVE
+class app00a {
+    ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+
+        while (curr != null) {
+            next = curr.next;// WE store the next before only so that we donot loose the next pointer while
+                             // changing the current's next to prev
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+
+    }
+}
+
+// RECURSIVE
+class app00b {
+    ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        // all the nodes will be pushed in recursi call stack and while removing them
+        // they will be in reverse order
+        // we just need to point current's next's next to current ie, curr.next.next =
+        // curr
+        // this will reverse the links between the nodes
+        ListNode temp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+}
---
+++ 05.LinkedList/helpers/ListNode.java
@@ -0,0 +1,11 @@
+package helpers;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int val) {
+        val = this.val;
+        next = null;
+    }
+}
